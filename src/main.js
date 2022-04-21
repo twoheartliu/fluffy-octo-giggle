e5fb31418e67595808323e2e87b762ac2b503b1f
twoheart <twoheart@vip.qq.com>
2022-04-21 18:59:33 +0800

    feat: home components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import TyepNav from '@/pages/Home/TypeNav'
 
 Vue.config.productionTip = false
+Vue.component(TyepNav.name, TyepNav)
 
 new Vue({
   render: (h) => h(App),
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>index page</div>
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+  </div>
 </template>
 
 <script>
-export default {
+import ListContainer from './ListContainer'
+import Recommend from './Recommend'
 
+export default {
+  components: {
+    ListContainer, Recommend
+  }
 }
 </script>
 
